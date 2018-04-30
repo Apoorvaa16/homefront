b5bb2117d6f63c0c141ca4d0a2b8b64fc7745333
Apoorvaa16 <37121801+Apoorvaa16@users.noreply.github.com>
2018-04-30 13:29:59 -0400

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">  
+function showIt() {  
+  document.getElementById("hide").style.visibility = "visible";  
+}  
+setTimeout("showIt()", 30000); // after 30 sec  
+</script>  
